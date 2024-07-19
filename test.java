c10bcb04311ec40448535061e3efbac754ec4c5e
Andi <andi.it@unpam.ac.id>
2024-07-19 18:56:06 +0700

    Text dalam java

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class Makan{
   public static void main (String []args){
   System.Out.Println("Hello Word!...");
- System.Out.Println("Hello Word!...");
+System.Out.Println("Hello Word!...");
+System.Out.Println("Hello Word!...");
 }
 }
